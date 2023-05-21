4ef3966f2594c87df6056d457d89fc9f9ed1b843
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-05-21 19:04:35 +0500

    changed swagger settings

--- serp_checker/settings.py
+++ serp_checker/settings.py
@@ -124,10 +124,6 @@ STATIC_URL = 'static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
-SWAGGER_SETTINGS = {
-    'USE_SESSION_AUTH': False
-}
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
