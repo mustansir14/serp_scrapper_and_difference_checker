fc416cbaef4d09da15c7aa139074b474da614509
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-05-21 18:48:41 +0500

    created static folder

--- serp_checker/settings.py
+++ serp_checker/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
