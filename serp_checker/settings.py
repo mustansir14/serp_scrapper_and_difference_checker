895f545cb3f187bfe8e6e256b4ab0e6903bb4bc7
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-05-21 17:03:46 +0500

    added db to env

--- serp_checker/settings.py
+++ serp_checker/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from celery.schedules import crontab
 from datetime import timedelta
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +81,7 @@ WSGI_APPLICATION = 'serp_checker.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
