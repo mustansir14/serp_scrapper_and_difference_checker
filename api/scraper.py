4d11338b10498a3a2a5f97baf342512f9a93e473
Mustansir Muzaffar <mustansir2001@gmail.com>
2023-07-21 23:25:50 +0500

    added timeout to request

--- api/scraper.py
+++ api/scraper.py
@@ -203,6 +203,7 @@ class Scraper:
             headers={
                 "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
             },
+            timeout=30
         )
         soup = BeautifulSoup(res.content, "html.parser")
         return soup.text
